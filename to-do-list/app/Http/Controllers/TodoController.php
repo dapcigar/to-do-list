cbc6783ca11d019e6868335ce91a0ac9ee8199d5
Oladapo Alade <dapcigar@gmail.com>
2023-11-25 12:18:21 +0000

    view all control

--- to-do-list/app/Http/Controllers/TodoController.php
+++ to-do-list/app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
@@ -11,7 +12,10 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::get();
+        //dd($todos);
+
+        return view('index', ['todos' => $todos]);
     }
 
     /**
